b46724e893a0c2a563414431a00b5e231442626f
vincentgraillat <vincent@nelson-mobility.com>
2025-11-26 21:48:07 +0100

    Create expense model

---
+++ backend/models/expense.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from uuid import uuid4
+
+from .currency import Currency
+
+
+@dataclass
+class Expense:
+    id: str = field(default_factory=lambda: str(uuid4()))
+    description: str = ""
+    amount: float = 0.0
+    currency: Currency = Currency.EUR
+
+    payer_id: str = ""
+    participants_ids: list[str] = field(default_factory=list)
+
+    def split_amount(self) -> float:
+        if not self.participants_ids:
+            return 0.0
+        return self.amount / len(self.participants_ids)
